2e82b35f27355f2830eca8b534dacc42a3bff9cc
Divyanshirastogi <divyanshirastogi22@gmail.com>
2016-11-08 15:53:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+    user : 'divyanshirastogi',
+    database :	'divyanshirastogi',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password :process.env .DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles ={ };
+
+function createTemplate (data) { }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('test-db', function (req,res) {
+    //make a select request
+    //return aresponse with a result
+    pool.query('SELECT * 'FROM test', function (err, result) {
+      if( err) {
+          res.status(500).send(err.toString));
+          } else {
+             res.send(JSON.stringify(result)) ;
+          }
+    });
+    });
+
 var counter =0;
 app.get('/counter',function (req,res){
     counter=counter+1;
-    res.send(counter.toString());
+    res.send(counter.toString l
 });
 
-app.get('/article-one' , function (req,res) { 
- res.sendFile(path.join(__dirname, 'ui', 'article one.html'));
-});
-
-app.get('/article-two' , function (req,res) {
- res.send('Article two requested & will be send here');
-});
-
-app.get('/article-three' , function (req,res){ 
- res.send('Article three requested & will be send here');
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 var names=[];
 app.get('/submit-name/:name',function(req.res){
@@ -43,6 +52,12 @@ app.get('/submit-name/:name',function(req.res){
     //JSON:javascript object notation
     res.send(JSON.stringify(names));
 });
+app.get('/:articleName' , function (req,res) {
+//articleName==article-one
+//articles[articleName] =={} content object for article-one
+var articleName = req.params.articleName;
+res.send(createTemplate (articles[articleName] ));
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
